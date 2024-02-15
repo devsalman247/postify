67cf6ca3991886239f7210605e329ce0e6966530
devsalman247 <salman247.dev@gmail.com>
2024-02-15 17:49:09 +0500

    Initial commit

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,15 +2,24 @@ import { Component, inject, signal } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { UserService } from '../../services/user.service';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+export type IToast = {
+  type: string;
+  message: string;
+  title?: string;
+  timeOut?: number;
+};
 
 @Component({
   selector: 'login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  isValidatingForm = false;
   isLoggedIn = signal(false);
 
   fb = inject(FormBuilder);
@@ -18,31 +27,45 @@ export class LoginComponent {
   userService = inject(UserService);
 
   isLoading: Boolean = false;
-  formData = this.fb.nonNullable.group({
-    email: ['', Validators.required, Validators.email],
+  formData = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
     password: ['', Validators.required],
   });
 
   onSubmit() {
+    if (!this.isValidatingForm) this.isValidatingForm = true;
     if (this.isLoading) return;
 
     if (this.formData.valid) {
       this.isLoading = true;
+      this.toaster.clear();
       const { email, password } = this.formData.value;
 
       this.userService.login(email as string, password as string).subscribe({
         next: (response) => {
-          console.log(response);
-          this.toaster.success('Login successful!');
-          this.isLoading = false;
+          if (Array.isArray(response) && response.length > 0) {
+            this.showToast({ type: 'success', message: 'Login successful!' });
+          } else {
+            this.showToast({ type: 'error', message: 'User not found!' });
+          }
         },
         error: (error) => {
-          this.toaster.error('Login failed!');
-          this.isLoading = false;
+          this.showToast({ type: 'error', message: 'Login failed!' });
+        },
+        complete: () => {
+          setTimeout(() => {
+            this.isLoading = false;
+          }, 1000);
         },
       });
-    } else {
-      this.formData.markAllAsTouched();
     }
+  }
+
+  showToast({ type, message, title = '', timeOut = 20000 }: IToast) {
+    if (type === 'error') {
+      this.toaster.error(message, title, { timeOut });
+      return;
+    }
+    this.toaster.success(message, title, { timeOut });
   }
 }
